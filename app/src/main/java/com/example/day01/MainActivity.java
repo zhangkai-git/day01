c8c75891f3568210ebc6bc0721d8df2cd389c616
zhangkai-git <728699443@qq.com>
2020-12-15 16:31:16 +0800

    第一次

---
+++ app/src/main/java/com/example/day01/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.day01;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "吐司", Toast.LENGTH_SHORT).show();
+    }
+}
